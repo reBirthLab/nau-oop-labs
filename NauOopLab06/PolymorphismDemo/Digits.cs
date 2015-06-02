8731fd39e2fab20658913c4de6a01b53f8911aa8
Anastasiy Tovstik <archianastasius@gmail.com>
2015-06-02 19:42:13 +0300

    Classes created

---
+++ NauOopLab06/PolymorphismDemo/Digits.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismDemo
+{
+    class Digits : Strings
+    {
+        Strings s;
+
+        public Digits(String str) : base(str)
+        {
+        }
+
+    }
+}
---
+++ NauOopLab06/PolymorphismDemo/Strings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismDemo
+{
+    class Strings
+    {
+        protected String str;
+
+        public Strings(String str)
+        {
+            this.str = str;
+        }
+
+        public virtual int length()
+        {
+            return str.Length;
+        }
+    }
+}
