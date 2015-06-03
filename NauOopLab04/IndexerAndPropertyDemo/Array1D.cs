731b631c709144d3dec0cb614aafbc5ddfac644f
Anastasiy Tovstik <archianastasius@gmail.com>
2015-06-03 14:19:19 +0300

    Complete lab

---
+++ NauOopLab04/IndexerAndPropertyDemo/Array1D.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerAndPropertyDemo
+{
+    class Array1D
+    {
+        int[] array;
+        private int size;
+        public bool outOfRange;
+
+        // Constructor
+        public Array1D(int size)
+        {
+            array = new int[size];
+            this.size = size;
+        }
+
+        // Checks if index in range
+        private bool inRange(int index)
+        {
+            if (index >= 0 & index < size)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        // Indexer
+        public int this[int index]
+        {
+            get
+            {
+                if (inRange(index))
+                {
+                    outOfRange = false;
+                    return array[index];
+                }
+                else
+                {
+                    outOfRange = true;
+                    return 0;
+                }
+            }
+
+            set
+            {
+                if (inRange(index))
+                {
+                    array[index] = value;
+                    outOfRange = false;
+                }
+                else
+                {
+                    outOfRange = true;
+                }
+
+            }
+        }
+
+        // Property
+        public int Size
+        {
+            get
+            {
+                return size;
+            }
+
+            set
+            {
+                if(value >= 0)
+                {
+                    size = value;
+                    array = new int[size];
+                }
+            }
+        }
+
+    }
+}
--- NauOopLab04/IndexerAndPropertyDemo/Program.cs
+++ NauOopLab04/IndexerAndPropertyDemo/Program.cs
@@ -10,6 +10,67 @@ namespace IndexerAndPropertyDemo
     {
         static void Main(string[] args)
         {
+            int n = 5;
+            int x;
+
+            Array1D myArray = new Array1D(n);
+
+            Random rand = new Random();
+
+            Console.WriteLine("ARRAY INITIALIZATION");
+            for(int i = 0; i < n*2; i++)
+            {
+                myArray[i] = rand.Next(100);
+
+                if (myArray.outOfRange)
+                {
+                    Console.WriteLine("Index {0}: Value is out of range", i);
+                }           
+            }
+
+            Console.WriteLine("\nPRINTOUT OF ARRAY CONTENT");
+            for (int i = 0; i < n*2; i++)
+            {
+                x = myArray[i];
+
+                if (!myArray.outOfRange)
+                {
+                    Console.WriteLine("Index {0}: {1}", i, x);                   
+                }
+                else
+                {
+                    Console.WriteLine("Index {0}: Value is out of range", i);
+                }                
+            }
+
+            myArray.Size = 10;
+            Console.WriteLine("\nEXTENDED ARRAY INITIALIZATION");
+            for (int i = 0; i < n * 2; i++)
+            {
+                myArray[i] = rand.Next(100);
+
+                if (myArray.outOfRange)
+                {
+                    Console.WriteLine("Index {0}: Value is out of range", i);
+                }
+            }
+
+            Console.WriteLine("\nPRINTOUT OF EXTENDED ARRAY CONTENT");
+            for (int i = 0; i < n * 2; i++)
+            {
+                x = myArray[i];
+
+                if (!myArray.outOfRange)
+                {
+                    Console.WriteLine("Index {0}: {1}", i, x);
+                }
+                else
+                {
+                    Console.WriteLine("Index {0}: Value is out of range", i);
+                }
+            }
+
+            Console.Read();
         }
     }
 }
